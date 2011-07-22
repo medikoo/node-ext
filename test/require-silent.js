c018aa69dddf1608c45042db08937a9de7f4a4d5
Mariusz Nowak <medikoo@medikoo.com>
2011-07-22 09:01:25 +0200

    requireSilent didn't return error on internal require errors. Tests

--- test/require-silent.js
+++ test/require-silent.js
@@ -2,17 +2,16 @@
 
 var playground = __dirname + '/__playground';
 
-module.exports = {
-	"Existing": function (t, a) {
-		var file = playground + '/sample';
-		a.equal(t(require)(file), require(file));
-	},
-	"Non existing": function (t, a) {
-		var file = playground + '/sample-na';
-		a.strictEqual(t(require)(file), null);
-	},
-	"With evaluation error": function (t, a) {
-		var file = playground + '/sample-error';
-		a.ok(t(require)(file) instanceof Error);
-	}
+module.exports = function (t, a) {
+	var file = playground + '/sample';
+	a(t(require)(file), require(file), "Existing");
+
+	file = playground + '/sample-na';
+	a(t(require)(file), null, "Non existing");
+
+	file = playground + '/sample-error';
+	a.ok(t(require)(file) instanceof Error, "Evaluation error");
+
+	file = playground + '/require-error';
+	a.ok(t(require)(file) instanceof Error, "Internal require error");
 };
