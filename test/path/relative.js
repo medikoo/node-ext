dbe3e036c458831fe28b21659a6cab8c95c1c07a
Mariusz Nowak <mariusz@medikoo.com>
2012-03-20 22:49:08 +0100

    Fix tests for Windows

--- test/path/relative.js
+++ test/path/relative.js
@@ -1,25 +1,28 @@
 'use strict';
 
+var join = require('path').join;
+
 module.exports = {
 	"Same paths": function (t, a) {
 		a.equal(t('/a/b/c', '/a/b/c'), '');
 	},
 	"From deeper than to": function (t, a) {
-		a.equal(t('/a/b/c/d/', '/a/b/'), '../../');
+		a.equal(t('/a/b/c/d/', '/a/b/'), join('../../'));
 	},
 	"From deeper than to (file)": function (t, a) {
-		a.equal(t('/a/b/c/d/', '/a/b/x'), '../../x');
+		a.equal(t('/a/b/c/d/', '/a/b/x'), join('../../x'));
 	},
 	"To deeper than from": function (t, a) {
-		a.equal(t('/a/b/', '/a/b/c/d/'), 'c/d/');
+		a.equal(t('/a/b/', '/a/b/c/d/'), join('c/d/'));
 	},
 	"To deeper than from (file)": function (t, a) {
-		a.equal(t('/a/b/', '/a/b/c/d/x'), 'c/d/x');
+		a.equal(t('/a/b/', '/a/b/c/d/x'), join('c/d/x'));
 	},
 	"Different paths": function (t, a) {
-		a.equal(t('/a/b/c/', '/e/f/g/'), '../../../e/f/g/');
+		a.equal(t('/a/b/c/', '/e/f/g/'), join('../../../e/f/g/'));
 	},
 	"CWD": function (t, a) {
-		a.equal(t(__dirname + '/a/b/'), t(process.cwd(), __dirname + '/a/b/'));
+		a.equal(t(__dirname + '/a/b/'), t(process.cwd(),
+			__dirname + join('/a/b/')));
 	}
 };
