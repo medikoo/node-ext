71e2b0aab66e534156dd9c71bcb946cfdeacd2ac
Mariusz Nowak <mariusz@medikoo.com>
2012-05-30 08:23:24 +0200

    fs.isIgnored - Whether file is ignored by pattern
    Patterns are read from ignorefiles like .gitignore

---
+++ test/fs/is-ignored.js
@@ -0,0 +1,50 @@
+'use strict';
+
+var resolve = require('path').resolve
+
+  , pgPath = resolve(__dirname, '../__playground/dirscan');
+
+module.exports = function (t) {
+	return {
+		"Not ignored": function (a, d) {
+			t(resolve(pgPath, 'eleven'), '.ignore')(function (r) {
+				a(r, false);
+			}).end(d);
+		},
+		"Ignored absolute": function (a, d) {
+			t(resolve(pgPath, 'six'), '.ignore')(function (r) {
+				a(r, true);
+			}).end(d);
+		},
+		"Ignored relative": function (a, d) {
+			t(resolve(pgPath, 'five'), '.ignore')(function (r) {
+				a(r, true);
+			}).end(d);
+		},
+		"Ignored by pattern": function (a, d) {
+			t(resolve(pgPath, 'nine.foo'), '.ignore')(function (r) {
+				a(r, true);
+			}).end(d);
+		},
+		"Excluded": function (a, d) {
+			t(resolve(pgPath, 'nine.keep'), '.ignore')(function (r) {
+				a(r, false);
+			}).end(d);
+		},
+		"Ignored deep": function (a, d) {
+			t(resolve(pgPath, 'one/five'), '.ignore')(function (r) {
+				a(r, true);
+			}).end(d);
+		},
+		"Not ignored deep": function (a, d) {
+			t(resolve(pgPath, 'one/ten'), '.ignore')(function (r) {
+				a(r, false);
+			}).end(d);
+		},
+		"ignored in deep": function (a, d) {
+			t(resolve(pgPath, 'one/seven'), '.ignore')(function (r) {
+				a(r, true);
+			}).end(d);
+		}
+	}
+};
