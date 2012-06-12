a2cc8d2f952fe2d12cb22a830fb6eab8fcfa543a
Mariusz Nowak <mariusz@medikoo.com>
2012-06-12 18:38:56 +0200

    Reworked fs.watchOnce into fs.watchPath
    It allows watching given paths no matter whether they exist or not.
    It fires 'create', 'change' and 'remove' events

---
+++ test/fs/watch-path.js
@@ -0,0 +1,77 @@
+'use strict';
+
+var fs        = require('fs')
+  , resolve   = require('path').resolve
+  , deferred  = require('deferred')
+  , delay     = deferred.delay
+  , promisify = deferred.promisify
+  , mkdir     = promisify(fs.mkdir)
+  , open      = promisify(fs.open)
+  , write     = promisify(fs.write)
+  , close     = promisify(fs.close)
+  , writeFile = promisify(fs.writeFile)
+  , unlink    = promisify(fs.unlink)
+  , rmdir     = promisify(fs.rmdir)
+
+  , pgPath = resolve(__dirname, '../__playground/watch')
+
+module.exports = function (t, a, d) {
+	var ondirchange = [], onfilechange = [], tmpPath, tmpFilePath, alt
+	  , dirPath = resolve(pgPath, 'tmp')
+	  , filePath = resolve(dirPath, 'tmp')
+	  , dirCurrent, fileCurrent
+
+	t(dirPath).on('change', function (e) {
+		// console.log("RECEIVED: DIR CHANGE", e.type);
+		ondirchange.push(e.type);
+	});
+	t(filePath).on('change', function (e) {
+		// console.log("RECEIVED: FILE CHANGE", e.type);
+		onfilechange.push(e.type);
+	});
+
+	delay(function () {
+		return mkdir(dirPath);
+	}, 10)()(delay(function () {
+		a(ondirchange.shift(), 'create', "Dir: Dir created");
+		a(onfilechange.shift(), undefined, "File: Dir created");
+		return writeFile(filePath, 'raz');
+	}, 10))(delay(function () {
+		a(ondirchange.shift(), 'change', "Dir: File created");
+		a(onfilechange.shift(), 'create', "File: File created");
+		return open(filePath, 'a')(function (fd) {
+			return write(fd, new Buffer('dwatrzy'), 0, 3, null)(function () {
+				return close(fd);
+			});
+		});
+	}, 10))(delay(function () {
+		a(ondirchange.shift(), undefined, "Dir: File changed");
+		a(onfilechange.shift(), 'change', "File: File changed");
+		return unlink(filePath);
+	}, 10))(delay(function () {
+		a(ondirchange.shift(), 'change', "Dir: File removed");
+		a(onfilechange.shift(), 'remove', "File: File removed");
+		return rmdir(dirPath);
+	}, 10))(delay(function () {
+		a(ondirchange.shift(), 'remove', "Dir: Dir removed");
+		a(onfilechange.shift(), undefined, "File: Dir removed");
+		return mkdir(dirPath);
+	}, 10))(delay(function () {
+		a(ondirchange.shift(), 'create', "Dir: Dir created #2");
+		a(onfilechange.shift(), undefined, "File: Dir created #2");
+		return writeFile(filePath, 'raz');
+	}, 10))(delay(function () {
+		a(ondirchange.shift(), 'change', "Dir: File created #2");
+		a(onfilechange.shift(), 'create', "File: File created #2");
+		return unlink(filePath);
+	}, 10))(delay(function () {
+		a(ondirchange.shift(), 'change', "Dir: File removed #2");
+		a(onfilechange.shift(), 'remove', "File: File removed #2");
+		return rmdir(dirPath);
+	}, 10))(delay(function () {
+		a(ondirchange.shift(), 'remove', "Dir: Dir removed #2");
+		a(onfilechange.shift(), undefined, "File: Dir removed #2");
+		a.deep(ondirchange, [], "Dir: Extra events");
+		a.deep(onfilechange, [], "File: Extra events");
+	}, 10)).end(d);
+};
