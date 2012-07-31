dc722f20aa0acf85dd48193deed1a736f926e735
Mariusz Nowak <mariusz@medikoo.com>
2012-07-31 16:27:16 +0200

    Remove obsolete path.trim module

--- test/path/trim.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = function (t, a) {
-	a(t('raz/dwa/'), 'raz/dwa', "With ending slash");
-	a(t('/raz/dwa'), '/raz/dwa', "Without ending slash");
-	a(t(''), '', "Empty");
-	a(t('/'), '/', "Root");
-};
