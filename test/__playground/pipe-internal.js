25b1a00b8008bff88505ad0f8dbda45c1b6605aa
Mariusz Nowak <mariusz@medikoo.com>
2012-08-08 12:32:14 +0200

    Removed no longer used test playground files

--- test/__playground/pipe-internal.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-
-'use strict';
-
-process.stdout.write('STDOUT\n');
-process.stderr.write('STDERR\n');
--- test/__playground/pipe-wrapper.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-
-'use strict';
-
-var spawn = require('child_process').spawn
-  , pipe  = require('../../lib/child_process/pipe');
-
-pipe(spawn(__dirname + '/pipe-internal.js'));
