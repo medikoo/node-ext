6f7614b32b5d29c6a012145f8f1f58fe0ab798d1
Mariusz Nowak <mariusz@medikoo.com>
2012-09-04 16:53:13 +0200

    descriptorLimiter is now a function
    descriptorLimiter takes callback that will be called only when there
    would free descriptor slots

--- test/fs/_descriptor-limiter.js
+++ test/fs/_descriptor-limiter.js
@@ -3,7 +3,7 @@
 module.exports = function (t, a) {
 	var invoked;
 	a(t.isAvailable(), true, "Available at start");
-	t.cb(function () { invoked = true; });
+	t(function () { invoked = true; });
 	a(invoked, true, "Callback");
 	t.open();
 	t.close();
