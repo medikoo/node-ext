bb6fbd73dfb9783d1bce434c4c624b6478b8fc2a
Mariusz Nowak <mariusz@medikoo.com>
2012-09-29 23:36:30 +0200

    Expose descriptorHandler on index

--- test/fs/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var indexTest = require('tad/lib/utils/index-test')
-  , dir       = require('path').resolve(__dirname, '../../lib/fs');
-
-module.exports = indexTest(indexTest.readDir(dir)(function (o) {
-	delete o.descriptorLimiter;
-	return o;
-}));
